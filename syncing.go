1f0a4563853fe8c64535c39448b469d9be655637
Valentyn Nastenko <versus.ua@gmail.com>
2018-02-12 18:39:56 +0200

    add syncing percent formula

---
+++ syncing.go
@@ -0,0 +1,24 @@
+package main
+
+
+/*
+
+var lastPercentage = 0;var lastBlocksToGo = 0;var timeInterval = 10000;
+setInterval(function(){
+    var percentage = eth.syncing.currentBlock/eth.syncing.highestBlock*100;
+    var percentagePerTime = percentage - lastPercentage;
+    var blocksToGo = eth.syncing.highestBlock - eth.syncing.currentBlock;
+    var bps = (lastBlocksToGo - blocksToGo) / (timeInterval / 1000)
+    var etas = 100 / percentagePerTime * (timeInterval / 1000)
+
+    var etaM = parseInt(etas/60,10);
+    console.log(parseInt(percentage,10)+'% ETA: '+etaM+' minutes @ '+bps+'bps');
+
+    lastPercentage = percentage;lastBlocksToGo = blocksToGo;
+},timeInterval);
+
+
+
+https://hiddentao.com/archives/2016/05/04/setting-up-geth-ethereum-node-to-run-automatically-on-ubuntu/
+
+ */
