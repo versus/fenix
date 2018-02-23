f23cff40cb580da226176ca0d52a409ab842375d
Valentyn Nastenko <versus.ua@gmail.com>
2018-02-23 20:40:56 +0200

    add snapshot check progress

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 
-	"gopkg.in/cheggaaa/pb.v1"
-	"github.com/joho/godotenv"
 	"log"
 
-	"os"
-	"time"
+	"github.com/joho/godotenv"
+	"gopkg.in/cheggaaa/pb.v1"
+
 	"flag"
 	"net/url"
+	"os"
 	"strings"
+	"time"
 )
 
 const (
@@ -26,7 +28,7 @@ const (
 //https://github.com/rancher/convoy/blob/master/ebs/ebs_service.go
 
 func main() {
-
+	var SnapshotId *string
 	log.Println("fenix ", Version, Author)
 	err := godotenv.Load()
 	if err != nil {
@@ -40,29 +42,28 @@ func main() {
 	log.Println(flagInstanceId)
 
 	go func() {
-	count := 100
-	bar := pb.StartNew(count)
-	bar.SetMaxWidth(80)
-	bar.ShowTimeLeft = false
-	for i := 0; i < count;  {
-		//bar.Increment()
-		bar.Add64(5)
-		i = i + 5
-		time.Sleep(1*time.Second)
-	}
-	bar.FinishPrint("The End!")
+		count := 100
+		bar := pb.StartNew(count)
+		bar.SetMaxWidth(80)
+		bar.ShowTimeLeft = false
+		for i := 0; i < count; {
+			//bar.Increment()
+			bar.Add64(5)
+			i = i + 5
+			time.Sleep(1 * time.Second)
+		}
+		bar.FinishPrint("The End!")
 	}()
 
 	log.Println(os.Getenv("AWS_ACCESS_KEY_ID"))
 
 	if *flagDry == false {
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))})
-	svc := ec2.New(sess)
-
+		sess, err := session.NewSession(&aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))})
+		svc := ec2.New(sess)
 
 		params := &ec2.DescribeInstancesInput{
 			Filters: []*ec2.Filter{
-				&ec2.Filter{
+				{
 					Name: aws.String("instance-state-name"),
 					Values: []*string{
 						aws.String("running"),
@@ -72,15 +73,14 @@ func main() {
 			},
 		}
 
-		// TODO: Actually care if we can't connect to a host
 		resp, err := svc.DescribeInstances(params)
 		if err != nil {
-		      panic(err)
-		 }
+			panic(err)
+		}
 		fmt.Println(resp)
 		// Loop through the instances. They don't always have a name-tag so set it
 		// to None if we can't find anything.
-		for idx, _ := range resp.Reservations {
+		for idx := range resp.Reservations {
 			for _, inst := range resp.Reservations[idx].Instances {
 
 				// We need to see if the Name is one of the tags. It's not always
@@ -93,8 +93,8 @@ func main() {
 				}
 
 				hdd := inst.BlockDeviceMappings
-                log.Println("count block devices ", len(hdd))
-                log.Println("dev 1: ", hdd[1].Ebs.VolumeId)
+				log.Println("count block devices ", len(hdd))
+				log.Println("dev 1: ", hdd[1].Ebs.VolumeId)
 				input := &ec2.CreateSnapshotInput{
 					Description: aws.String("This is data volume snapshot."),
 					VolumeId:    hdd[1].Ebs.VolumeId,
@@ -116,71 +116,61 @@ func main() {
 					return
 				}
 
-				for  i:=0; i< 10; i++ {
-					fmt.Println(res)
-					time.Sleep(2 * time.Second)
-				}
 				fmt.Println(res)
+				SnapshotId = res.SnapshotId
 
-				important_vals := []*string{
+				importantVals := []*string{
 					inst.InstanceId,
 					&name,
 					inst.PrivateIpAddress,
 					inst.InstanceType,
 					inst.PublicIpAddress,
-
-
 				}
 
 				// Convert any nil value to a printable string in case it doesn't
 				// doesn't exist, which is the case with certain values
-				output_vals := []string{}
-				for _, val := range important_vals {
+				var outputVals []string
+				for _, val := range importantVals {
 					if val != nil {
-						output_vals = append(output_vals, *val)
+						outputVals = append(outputVals, *val)
 					} else {
-						output_vals = append(output_vals, "None")
+						outputVals = append(outputVals, "None")
 					}
 				}
 				// The values that we care about, in the order we want to print them
-				fmt.Println(strings.Join(output_vals, " "))
+				fmt.Println(strings.Join(outputVals, " "))
 			}
 		}
 
-
-
-
-
-
-
-	snapInput := &ec2.DescribeSnapshotsInput{
-		SnapshotIds: []*string{
-			aws.String("snap-0f411b956abd7ece8"),
-		},
-	}
-
-	snapshots, err := svc.DescribeSnapshots(snapInput)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
+		snapInput := &ec2.DescribeSnapshotsInput{
+			SnapshotIds: []*string{
+				//aws.String("snap-0f411b956abd7ece8"),
+				SnapshotId,
+			},
 		}
-		return
-	}
 
-	fmt.Println(snapshots)
+		for i := 0; i < 10; i++ {
+			snapshots, err := svc.DescribeSnapshots(snapInput)
+			if err != nil {
+				if aerr, ok := err.(awserr.Error); ok {
+					switch aerr.Code() {
+					default:
+						fmt.Println(aerr.Error())
+					}
+				} else {
+					// Print the error, cast err to awserr.Error to get the Code and
+					// Message from an error.
+					fmt.Println(err.Error())
+				}
+				return
+			}
 
-	for indx, _ := range snapshots.Snapshots {
-		snap := snapshots.Snapshots[indx]
-		fmt.Println("snap SnapshotId =", snap.SnapshotId)
-	}
+			fmt.Println(snapshots)
+			if *snapshots.Snapshots[0].State == "completed" {
+				break
+			}
+			time.Sleep(2 * time.Second)
+		}
 
 	}
 }
-
